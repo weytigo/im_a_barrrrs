cdc12bb27afcab85a78578d092effb493007fffc
Винсент <133240290+weytigo@users.noreply.github.com>
2024-10-16 10:42:33 +0500

    Update task2.py
    щас бы покушать вкусно том ямчика

--- lab1/task2.py
+++ lab1/task2.py
@@ -1,1 +1,10 @@
+import math
 
+a=((x**3/2)^1/2)-math.sin(y)
+b=math.exp(2/3)-math.cos(y)+z+math.log(y)
+
+x=float(input("Введите значение переменной x: "))
+y=float(input("Введите значение переменной y: "))
+z=float(input("Введите значение переменной z: "))
+
+print(f"Получено значение функции a={a}")
